3d06dbc343b43d9211d86b7ebb9c4fe19e0bca8a
Stefano Salsano <stefano.salsano@uniroma2.it>
2024-07-02 15:40:11 +0200

    wip

--- src/transport/generic.cc
+++ src/transport/generic.cc
@@ -9,6 +9,7 @@ ncclResult_t ncclTransportRingConnect(struct ncclComm* comm) {
       NCCLCHECKGOTO(ncclTransportP2pConnect(comm, c, 1, &channel->ring.prev, 1, &channel->ring.next, 0), ret, fail);
     }
     NCCLCHECKGOTO(ncclTransportP2pSetup(comm, &comm->graphs[NCCL_ALGO_RING], 0), ret, fail);
+    INFO(NCCL_ALL,"ncclTransportP2pSetup ----------------DONE ncclTransportRingConnect------------------------>");
     INFO(NCCL_INIT, "Connected all rings");
   }
 exit:
@@ -27,6 +28,7 @@ ncclResult_t ncclTransportTreeConnect(struct ncclComm* comm) {
       NCCLCHECKGOTO(ncclTransportP2pConnect(comm, c, 1, &channel->tree.up, NCCL_MAX_TREE_ARITY, channel->tree.down, 0), ret, fail);
     }
     NCCLCHECKGOTO(ncclTransportP2pSetup(comm, &comm->graphs[NCCL_ALGO_TREE], 0), ret, fail);
+    INFO(NCCL_ALL,"ncclTransportP2pSetup ----------------DONE ncclTransportTreeConnect------------------------>")
     INFO(NCCL_INIT, "Connected all trees");
   }
 exit:
