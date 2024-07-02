cc95ec8149405a0165539ce7af2dd5034b4b527e
Stefano Salsano <stefano.salsano@uniroma2.it>
2024-07-02 17:48:54 +0200

    wip

--- src/device/prims_ll.h
+++ src/device/prims_ll.h
@@ -375,7 +375,9 @@ class Primitives<T, RedOp, Fan, Direct, ProtoLL, P2p>:
   }
 
   __device__ void send(intptr_t inpIx, int eltN) {
-      OUT ("%s","sendConn->hostname");
+      if (sendConn->hostname) {
+        OUT ("%s\n",sendConn->hostname);
+      }
       OUT ("thread: %d :GPU send inpIx %lu eltN %d \n",blockIdx.x * blockDim.x + threadIdx.x,inpIx,eltN);
     return LLGenericOp<0, 1, Input, -1>(inpIx, -1, eltN, false);
   }
