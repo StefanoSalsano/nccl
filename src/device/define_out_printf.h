fcb13ee1d3d86af89d0678d307931cd40aa8ef90
Stefano Salsano <stefano.salsano@uniroma2.it>
2024-07-03 13:51:43 +0200

    wip

--- src/device/define_out_printf.h
+++ src/device/define_out_printf.h
@@ -4,9 +4,26 @@
 //define ENABLE_OUT to enable debug printing in CUDA kernels
 #define ENABLE_OUT
 
+void add_hostname(const char* format, ...) {
+    
+    // Start processing the variable arguments
+    va_list args;
+    va_start(args, format);
+    
+    // Print the hostname
+    printf("%s: ", "hostname");
+    
+    // Print the formatted string
+    vprintf(format, args);
+    
+    // Clean up the variable argument list
+    va_end(args);
+}
+
 
 #ifdef ENABLE_OUT
-#define OUT(...) if (ncclShmem.hostname_shmem) printf("%s: ",ncclShmem.hostname_shmem); printf(__VA_ARGS__)
+//#define OUT(...) printf(__VA_ARGS__)
+#define OUT(...) add_hostname(__VA_ARGS__)
 #else
 #define OUT(...)
 #endif
